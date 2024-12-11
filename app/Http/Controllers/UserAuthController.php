cb1bbb2d905a029096ef1b80868c52fac8b1434d
Omar Montero <omar@grupoapok.com>
2024-12-11 01:09:04 -0400

    Added more authentication features

--- app/Http/Controllers/UserAuthController.php
+++ app/Http/Controllers/UserAuthController.php
@@ -2,11 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\LoginUserPostRequest;
 use App\Http\Requests\RegisterUserPostRequest;
 use App\Models\User;
+use Auth;
 use Hash;
-use Illuminate\Http\Request;
 use Illuminate\Http\Response;
+use Illuminate\Http\Request;
 
 
 class UserAuthController extends Controller
@@ -22,6 +24,28 @@ class UserAuthController extends Controller
         return response()->json([
             'message' => 'User Created',
         ], Response::HTTP_OK );
+    }
+
+    public function login(LoginUserPostRequest $request){
+        $loginUserData = $request->input();
+        $user = User::where('email',$loginUserData['email'])->first();
+        if(!$user || !Hash::check($loginUserData['password'],$user->password)){
+            return response()->json([
+                'message' => 'Invalid Credentials'
+            ],Response::HTTP_UNAUTHORIZED);
+        }
+        $token = $user->createToken($user->name.'-AuthToken')->plainTextToken;
+        return response()->json([
+            'access_token' => $token,
+        ]);
+    }
+
+    public function logout(Request $request){
+        auth()->user()->tokens()->delete();
+
+        return response()->json([
+        "message"=>"logged out"
+        ]);
     }
 
 }
